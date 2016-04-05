bc2de007b32154113ef3f2b0642341149b03c74e
Florian Gilde <fgilde@gmail.com>
2016-04-05 14:32:14 +0200

    ccd loc

---
+++ CCD_LocCount/CCD_LocCount/LocCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CCD_LocCount
+{
+    public static class LocCounter
+    {
+        public static int LOC(string sourceCode)
+        {
+            var sourceOnly = LöscheAlleKommentare(sourceCode);
+            var lines = ZerlegeInZeilen(sourceOnly);
+            var trimmedLines = LöscheAlleLeerzeilen(lines);
+
+            return trimmedLines.Length;
+        }
+
+        public static string[] LöscheAlleLeerzeilen(string[] lines)
+        {
+            return lines.Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+        }
+
+        public static string[] ZerlegeInZeilen(string sourceOnly)
+        {
+            return sourceOnly.Split('\n');
+        }
+
+        public static string LöscheAlleKommentare(string sourceCode)
+        {
+            var pattern = @"(@(?:""[^""]*"")+|""(?:[^""\r\n\\]+|\\.)*""|'(?:[^'\r\n\\]+|\\.)*')|//.*|/\*(?s:.*?)\*/";
+            return Regex.Replace(sourceCode, pattern, Environment.NewLine);
+        }
+    }
+}
---
+++ CCD_LocCount/CCD_LocCountTest/LocTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CCD_LocCount;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CCD_LocCountTest
+{
+    [TestClass]
+    public class LocTests
+    {
+        [TestMethod]
+        public void LoC_AcceptanceTest()
+        {
+            Assert.AreEqual(7, LocCounter.LOC("/// summary \n /// ...\n/// <...>\npublic void Test()\n        {\n	/* ... */\n	.\n	.\n	.\n	/* ... */ return a;\n\n        }"));
+            Assert.AreEqual(3, LocCounter.LOC("var a = 5;\n/*\n.\n.\n.\n*/\na = 6; /* FOO */\na = 7; // BAR;"));
+            Assert.AreEqual(2, LocCounter.LOC("Test /*\n34534\ntrzrtz */ e5zt"));
+        }
+        
+
+        [TestMethod]
+        public void TestZerlegeZeilen()
+        {
+            Assert.AreEqual(8, LocCounter.ZerlegeInZeilen("var a = 5;\n/*\n.\n.\n.\n*/\na = 6; /* FOO */\na = 7; // BAR;").Length);
+        }
+
+        [TestMethod]
+        public void TestLöscheAlleKommentare()
+        {
+            var source = "//Anton\nHallo\n/*Muh\nKuh\n*/";
+
+            var expected = "\r\n\nHallo\n\r\n";
+            var löscheAlleKommentare = LocCounter.LöscheAlleKommentare(source);
+            Assert.AreEqual(expected, löscheAlleKommentare);
+        }
+
+        [TestMethod]
+        public void TestLöscheAlleLeerzeilen()
+        {
+            Assert.AreEqual(3, LocCounter.LöscheAlleLeerzeilen(new[]
+            {
+                "",
+                "123",
+                "   ",
+                "456",
+                "789",
+                ""
+            }).Length);
+        }
+
+       
+
+    }
+}
