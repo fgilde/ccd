899e7049315266bc748e27454b9f5979e5990ad5
kl1mm <tobias.klimm@gmail.com>
2016-04-06 12:06:48 +0200

    Rasiert

--- CCD_LocCount/CCD_LocCount/LocCounter.cs
+++ CCD_LocCount/CCD_LocCount/LocCounter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -6,26 +7,41 @@ namespace CCD_LocCount
 {
     public static class LocCounter
     {
-        public static int LOC(string sourceCode)
+        public static void LocCountLinq(string path, Action<string> foundFileAction)
         {
-            var sourceOnly = DeleteAllComments(sourceCode);
-            var lines = ZerlegeInZeilen(sourceOnly);
-            var trimmedLines = LöscheAlleLeerzeilen(lines);
+            var found = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
+                .Select(fp => new FileInfo(fp))
+                .Where(fi => fi.Extension == ".cs")
+                .Select(fi => $"{fi.Name};{LOC(File.ReadAllText(fi.FullName))};{File.ReadAllLines(fi.FullName).Length}");
 
-            return trimmedLines.Length;
+            foreach (var f in found)
+                foundFileAction(f);
         }
 
-        public static string[] LöscheAlleLeerzeilen(string[] lines)
+        public static void LocCount(string path, Action<string> foundFileAction)
+        {
+            
+        }
+
+        public static int LOC(string sourceCode)
+        {
+            var sourceWithOutComments = DeleteComments(sourceCode);
+            var lines = DecomposeInLines(sourceWithOutComments);
+            lines = DeleteEmptyLines(lines);
+            return lines.Length;
+        }
+
+        public static string[] DeleteEmptyLines(string[] lines)
         {
             return lines.Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
         }
 
-        public static string[] ZerlegeInZeilen(string sourceOnly)
+        public static string[] DecomposeInLines(string sourceOnly)
         {
             return sourceOnly.Split('\n');
         }
 
-        public static string DeleteAllComments(string sourceCode)
+        public static string DeleteComments(string sourceCode)
         {
             var pattern = @"(@(?:""[^""]*"")+|""(?:[^""\r\n\\]+|\\.)*""|'(?:[^'\r\n\\]+|\\.)*')|//.*|/\*(?s:.*?)\*/";
             return Regex.Replace(sourceCode, pattern, Environment.NewLine);
