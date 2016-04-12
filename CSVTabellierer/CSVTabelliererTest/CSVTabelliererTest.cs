7329e8a3f7ce8e6659dedcc9666d71bf21cea990
maltewitt <malte.witt@cp.ag>
2016-04-12 09:25:39 +0200

    Vorbereitung für heutige CCD-Session

---
+++ CSVTabellierer/CSVTabelliererTest/CSVTabelliererTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSVTabelliererTest
+{
+    [TestClass]
+    public class CSVTabelliererTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
