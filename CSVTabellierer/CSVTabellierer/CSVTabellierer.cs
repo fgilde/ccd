5d631d99eec1960f20d04bd9c458c6e55d033d58
maltewitt <malte.witt@cp.ag>
2016-04-12 14:43:18 +0200

    ZerlegeInSpalten implementiert

--- CSVTabellierer/CSVTabellierer/CSVTabellierer.cs
+++ CSVTabellierer/CSVTabellierer/CSVTabellierer.cs
@@ -10,7 +10,24 @@ namespace CSVTabellierer
     {
         public static IEnumerable<string> Tabelliere(IEnumerable<string> CSV_zeilen)
         {
+            IEnumerable<string[]> tabelle = ZerlegeInSpalten(CSV_zeilen);
+            int[] maxSpaltenlängen = MaxSpaltenlängen(tabelle);
+            return GeneriereOutput(tabelle, maxSpaltenlängen);
+        }
+
+        private static IEnumerable<string> GeneriereOutput(IEnumerable<string[]> tabelle, int[] maxSpaltenlänge)
+        {
             throw new NotImplementedException();
+        }
+
+        private static int[] MaxSpaltenlängen(IEnumerable<string[]> tabelle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<string[]> ZerlegeInSpalten(IEnumerable<string> csvZeilen)
+        {
+            return csvZeilen.Select(z => z.Split(';'));
         }
     }
 }
--- CSVTabellierer/CSVTabelliererTest/CSVTabelliererTest.cs
+++ CSVTabellierer/CSVTabelliererTest/CSVTabelliererTest.cs
@@ -15,5 +15,17 @@ namespace CSVTabelliererTest
             var erwartet = new[] {"123|ABCD |01A|", "---+-----+---+", "ABC|HALLO|1  |"};
             CollectionAssert.AreEqual(erwartet, CSVTabellierer.CSVTabellierer.Tabelliere(zeilen).ToArray());
         }
+
+        [TestMethod]
+        public void TestZerlegeInSpalten()
+        {
+            var zeilen = new[] { "123;ABCD;01A", "ABC;HALLO;1" };
+            var zerlegeInSpalten = CSVTabellierer.CSVTabellierer.ZerlegeInSpalten(zeilen);
+
+            Assert.AreEqual(2, zerlegeInSpalten.Count());
+            Assert.IsTrue(zerlegeInSpalten.All(x=>x.Length == 3));
+            CollectionAssert.AreEqual(new[] { "123","ABCD","01A"}, zerlegeInSpalten.First());
+            CollectionAssert.AreEqual(new[] { "ABC", "HALLO", "1" }, zerlegeInSpalten.Last());
+        }
     }
 }
