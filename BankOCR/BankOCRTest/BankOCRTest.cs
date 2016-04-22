599c4712bf9e4bc008e2240bbdf877520367e2c8
maltewitt <malte.witt@cp.ag>
2016-04-22 12:04:36 +0200

    Initial BankOCR

---
+++ BankOCR/BankOCRTest/BankOCRTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankOCRTest
+{
+    [TestClass]
+    public class BankOCRTest
+    {
+        [TestMethod]
+        public void AcceptanceTest()
+        {
+        }
+    }
+}
