88c0672d6a1a74a47211ef9aa60ecd85c1f48ec3
maltewitt <malte.witt@cp.ag>
2016-05-10 14:39:48 +0200

    Projekt dublettenfinder

---
+++ DublettenFinder/DublettenFinder/Contract.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DublettenFinder
+{
+    interface IDublettenprüfung
+    {
+        IEnumerable<IDublette> Sammle_Kandidaten(string pfad);
+        IEnumerable<IDublette> Sammle_Kandidaten(string pfad, Vergleichsmodi modus);
+
+        IEnumerable<IDublette> Prüfe_Kandidaten(IEnumerable<IDublette> kandidaten);
+    }
+
+    interface IDublette
+    {
+        IEnumerable<string> Dateipfade { get; }
+    }
+
+    enum Vergleichsmodi
+    {
+        Größe_und_Name,
+        Größe
+    }
+}
