10aca3cbc3ec135bb6e2ea6dfef7a991c4816ede
Florian Gilde <fgilde@gmail.com>
2016-07-26 11:11:48 +0200

    Methode Zerlegen

--- Taschenrechner/Taschenrechner/TaschenrechnerBackend.cs
+++ Taschenrechner/Taschenrechner/TaschenrechnerBackend.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,17 @@ namespace Taschenrechner
 {
     public class TaschenrechnerBackend
     {
+        public void Berechne(char input, Action<string> anzeigen, Action<string> fehler)
+        {
+            
+        }
+
+        public void Zerlegen(char input, Action<char> zifferErkannt, Action<char> operatorErkannt)
+        {
+            if (char.IsDigit(input))
+                zifferErkannt(input);
+            else
+                operatorErkannt(input);
+        }
     }
 }
---
+++ Taschenrechner/UnitTestProject1/TaschenrechnerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taschenrechner;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TaschenrechnerTests
+    {
+        [TestMethod]
+        public void BerechnerTest()
+        {
+            var failedCalled = false;
+            var backend = new TaschenrechnerBackend();
+            backend.Berechne('6', s => Assert.AreEqual("6", s), s => Assert.Fail());
+            backend.Berechne('4', s => Assert.AreEqual("64", s), s => Assert.Fail());
+            backend.Berechne('%', s => Assert.AreEqual("64", s), s => Assert.Fail());
+            backend.Berechne('0', s => Assert.AreEqual("0", s), s => Assert.Fail());
+            backend.Berechne('=', s => Assert.AreEqual("64", s), s =>
+            {
+                failedCalled = true;
+                Assert.AreEqual("Division durch 0", s);
+            });
+            Assert.IsTrue(failedCalled,"Kein Fehler aufgerufen");
+            backend.Berechne('8', s => Assert.AreEqual("8", s), s => Assert.Fail());
+            backend.Berechne('=', s => Assert.AreEqual("8", s), s => Assert.Fail());
+        }
+
+        [TestMethod]
+        public void ZerlegenTest()
+        {
+            var backend = new TaschenrechnerBackend();
+            var zifferErkannt = false;
+            var operatorErkannt = false;
+            backend.Zerlegen('1', c => zifferErkannt= true, c=> operatorErkannt = true);
+            Assert.IsTrue(zifferErkannt);
+            Assert.IsFalse(operatorErkannt);
+
+            zifferErkannt = false;
+            operatorErkannt = false;
+            backend.Zerlegen('+', c => zifferErkannt = true, c => operatorErkannt = true);
+            Assert.IsFalse(zifferErkannt);
+            Assert.IsTrue(operatorErkannt);
+        }
+    }
+}
