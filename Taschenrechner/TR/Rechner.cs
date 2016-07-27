c72aa75d18ad589f2b42c662e864503da52ebd08
kl1mm <tobias.klimm@gmail.com>
2016-07-27 13:42:12 +0200

    new

---
+++ Taschenrechner/TR/Rechner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TR
+{
+    public class Rechner
+    {
+        private char @operator;
+
+        public Rechner()
+        {
+            @operator = '+';
+        }
+
+        private void Berechne(int zahl, Action<int> onSuccess, Action<string, int> onError)
+        {
+            
+        }
+
+        public void Berechne(char op, int zahl, Action<int> onSuccess, Action<string, int> onError)
+        {
+            Berechne(zahl, onSuccess, onError);
+            @operator = op;
+        }
+    }
+}
---
+++ Taschenrechner/TR/Zahl.cs
@@ -0,0 +1,22 @@
+﻿namespace TR
+{
+    public class Zahl
+    {
+        public int Wert { get; private set; }
+
+        public Zahl()
+        {
+            Zurücksetzen();
+        }
+
+        public void Zurücksetzen()
+        {
+            Wert = 0;
+        }
+
+        public void Erweitern(char ziffer)
+        {
+            Wert = int.Parse(Wert.ToString() + ziffer);
+        }
+    }
+}
